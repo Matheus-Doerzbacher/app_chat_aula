6ca71a2f966697bc3c1d12a4547aee09e32fbdba
Matheus Doerzbacher <matheus.doer@gmail.com>
2024-01-30 17:02:00 -0300

    final aula 378

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,3 +1,5 @@
+import 'package:app_chat_aula/components/messages.dart';
+import 'package:app_chat_aula/components/new_message.dart';
 import 'package:app_chat_aula/service/auth/auth_service.dart';
 import 'package:flutter/material.dart';
 
@@ -7,15 +9,50 @@ class ChatPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).primaryColor,
+        title: const Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('Chat page'),
-            TextButton(
-              onPressed: () => AuthService().logout(),
-              child: const Text('Logout'),
+            Text(
+              'Chat Page',
+              style: TextStyle(
+                color: Colors.white,
+              ),
             ),
+          ],
+        ),
+        actions: [
+          DropdownButton(
+            icon: Icon(
+              Icons.more_vert,
+              color: Theme.of(context).primaryIconTheme.color,
+            ),
+            items: const [
+              DropdownMenuItem(
+                value: 'logout',
+                child: Row(
+                  children: [
+                    Icon(Icons.exit_to_app),
+                    SizedBox(width: 10),
+                    Text('Sair')
+                  ],
+                ),
+              ),
+            ],
+            onChanged: (value) {
+              if (value == 'logout') {
+                AuthService().logout();
+              }
+            },
+          ),
+        ],
+      ),
+      body: const SafeArea(
+        child: Column(
+          children: [
+            Expanded(child: Messages()),
+            NewMessage(),
           ],
         ),
       ),
