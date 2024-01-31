878fa74af967f17303c7d5ca542bc3442b0b6ae9
Matheus Doerzbacher <matheus.doer@gmail.com>
2024-01-31 13:28:16 -0300

    Iniciando o Firebase no APP

--- lib/pages/auth_or_app_page.dart
+++ lib/pages/auth_or_app_page.dart
@@ -4,22 +4,35 @@ import 'package:app_chat_aula/pages/chat_page.dart';
 import 'package:app_chat_aula/pages/loading_page.dart';
 import 'package:app_chat_aula/service/auth/auth_service.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 class AuthOrAppPage extends StatelessWidget {
   const AuthOrAppPage({super.key});
 
+  Future<void> init(BuildContext context) async {
+    await Firebase.initializeApp();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: StreamBuilder<ChatUser?>(
-          stream: AuthService().userChanges,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const LoadingPage();
-            } else {
-              return snapshot.hasData ? const ChatPage() : const AuthPage();
-            }
-          }),
+    return FutureBuilder(
+      future: init(context),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const LoadingPage();
+        } else {
+          return StreamBuilder<ChatUser?>(
+            stream: AuthService().userChanges,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const LoadingPage();
+              } else {
+                return snapshot.hasData ? const ChatPage() : const AuthPage();
+              }
+            },
+          );
+        }
+      },
     );
   }
 }
