882ee5d28fcacbba09aa47ea0d260df5192f6d59
Matheus Doerzbacher <matheus.doer@gmail.com>
2024-01-31 17:31:42 -0300

    Adicionado o PushNotification ao projeot

--- lib/pages/auth_or_app_page.dart
+++ lib/pages/auth_or_app_page.dart
@@ -3,14 +3,18 @@ import 'package:app_chat_aula/pages/auth_page.dart';
 import 'package:app_chat_aula/pages/chat_page.dart';
 import 'package:app_chat_aula/pages/loading_page.dart';
 import 'package:app_chat_aula/service/auth/auth_service.dart';
+import 'package:app_chat_aula/service/notification/chat_notification_service.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
 
 class AuthOrAppPage extends StatelessWidget {
   const AuthOrAppPage({super.key});
 
   Future<void> init(BuildContext context) async {
     await Firebase.initializeApp();
+    // ignore: use_build_context_synchronously
+    await Provider.of<ChatNotificationService>(context, listen: false).init();
   }
 
   @override
