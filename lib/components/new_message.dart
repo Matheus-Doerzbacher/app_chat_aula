4dc7468af564820a3a242ff41d764075b852ebd7
Matheus Doerzbacher <matheus.doer@gmail.com>
2024-01-30 21:45:00 -0300

    final aula 385

--- lib/components/new_message.dart
+++ lib/components/new_message.dart
@@ -1,12 +1,54 @@
+import 'package:app_chat_aula/service/auth/auth_service.dart';
+import 'package:app_chat_aula/service/chat/chat_service.dart';
 import 'package:flutter/material.dart';
 
-class NewMessage extends StatelessWidget {
+class NewMessage extends StatefulWidget {
   const NewMessage({super.key});
 
   @override
+  State<NewMessage> createState() => _NewMessageState();
+}
+
+class _NewMessageState extends State<NewMessage> {
+  String _message = '';
+  final _messageController = TextEditingController();
+
+  Future<void> _sendMessage() async {
+    final user = AuthService().currentUser;
+    if (user != null) {
+      await ChatService().save(_message, user);
+      _messageController.clear();
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Nova Mensagem'),
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: Container(
+              margin: const EdgeInsets.symmetric(horizontal: 10),
+              child: TextField(
+                controller: _messageController,
+                onChanged: (msg) => setState(() => _message = msg),
+                decoration: const InputDecoration(
+                  labelText: 'Enviar Mensagem...',
+                ),
+                onSubmitted: (_) {
+                  if (_message.trim().isNotEmpty) {
+                    _sendMessage();
+                  }
+                },
+              ),
+            ),
+          ),
+          IconButton(
+            onPressed: _message.trim().isEmpty ? null : _sendMessage,
+            icon: const Icon(Icons.send),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/service/chat/chat_mock_service.dart
+++ lib/service/chat/chat_mock_service.dart
@@ -5,32 +5,7 @@ import 'package:app_chat_aula/models/chat_user.dart';
 import 'package:app_chat_aula/service/chat/chat_service.dart';
 
 class ChatMockService implements ChatService {
-  static final List<ChatMessage> _msgs = [
-    ChatMessage(
-      id: '1',
-      text: 'Bom diia!!',
-      createdAt: DateTime.now(),
-      userId: '1',
-      userName: 'Tabata',
-      userImageURL: 'assets/images/avatar.png',
-    ),
-    ChatMessage(
-      id: '2',
-      text: 'Bom dia meu povo!',
-      createdAt: DateTime.now(),
-      userId: '312',
-      userName: 'Lucas',
-      userImageURL: 'assets/images/avatar.png',
-    ),
-    ChatMessage(
-      id: '3',
-      text: 'Bom dia galera',
-      createdAt: DateTime.now(),
-      userId: '456',
-      userName: 'Mica',
-      userImageURL: 'assets/images/avatar.png',
-    ),
-  ];
+  static final List<ChatMessage> _msgs = [];
   static MultiStreamController<List<ChatMessage>>? _controller;
   static final _msgsStream = Stream<List<ChatMessage>>.multi((controller) {
     _controller = controller;
@@ -54,7 +29,7 @@ class ChatMockService implements ChatService {
     );
 
     _msgs.add(newMessage);
-    _controller?.add(_msgs);
+    _controller?.add(_msgs.reversed.toList());
     return newMessage;
   }
 }
